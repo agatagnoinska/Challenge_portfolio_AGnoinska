e8413e630c9a243b9456db29f02b1b96ca716cae
agatagnoinska <agata.gnoinska@gmail.com>
2023-04-16 17:14:27 +0200

    selektory

--- pages/add_a_match_form.py
+++ pages/add_a_match_form.py
@@ -4,12 +4,14 @@ class Dashboard(BasePage):
     menu_scouts_panel_button_xpath = "//*[@aria-label='menu']"
     main_page_button_xpath = "//*[@id='__next']/div[1]/div/div/div/ul[1]/div[1]"
     players_button_xpath = "//*[@id='__next']/div[1]/div/div/div/ul[1]/div[2]"
-    language_button_xpath = "//*[@id='__next']/div[1]/div/div/div/ul[2]/div[1]"
-    sign_out_button_xpath = "//*[@id='__next']/div[1]/div/div/div/ul[2]/div[2]"
-    download_csv_button.xpath = "//*[contains(@aria-label, 'CSV')]"
-    print_button.xpath = "//*[contains( @ aria - label,'Print')]"
-    view_columns_button-xpath = "//*[contains(@data-testid, 'olumn')]"
-    filter_table_button_xpath = "//*[contains(@data-testid, 'Filt')]"
-    next_page_button_xpath= "//*[@id='pagination-next']"
+    matches_button_xpath= "//*[@id='__next']/div[1]/div/div/div/ul[2]/div[2]"
+    reports_button_xpath = "//*[@id='__next']/div[1]/div/div/div/ul[2]/div[3]"
+    language_button_xpath = "//*[@id='__next']/div[1]/div/div/div/ul[3]/div[1]"
+    sign_out_button_xpath = "//*[@id='__next']/div[1]/div/div/div/ul[3]/div[2]"
+    my_team_field_xpath = "//*[@name='myTeam']"
+    enemy_field_xpath = "//*[@name='enemyTeam']"
+    my_team_score_xpath = "//*[@name='myTeamScore']"
+    enemy_team_score_xpath = "//*[@name='enemyTeamScore']"
+
 
 pass
